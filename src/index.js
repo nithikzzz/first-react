19030ed72a1a2d1fbcbbccb4fca3ba30ebec8799
NithiyaPriya <nithiyapriya446@gmail.com>
2023-06-15 13:28:32 +0530

    commit deploy

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode basename="https://nithikzzz.github.io/first-react" >
-    <HashRouter >
+  <React.StrictMode >
+    <HashRouter basename="https://nithikzzz.github.io/first-react" >
     <App />
     </HashRouter>
   </React.StrictMode>
